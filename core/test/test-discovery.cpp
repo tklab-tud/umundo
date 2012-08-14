246cdd039de82a22a55171149a6ff0df750a9d55
Stefan Radomski <radomski@tk.informatik.tu-darmstadt.de>
2012-08-15 01:36:42 +0200

    some more test output

--- core/test/test-discovery.cpp
+++ core/test/test-discovery.cpp
@@ -45,6 +45,7 @@ int main(int argc, char** argv, char** envp) {
 	Discovery::add(testDiscoverable);
 	while(receives < 1)
 		UMUNDO_WAIT(monitor);
+	std::cout << "Successfully found node via discovery" << std::endl;
 
 	// test node / publisher / subscriber churn
 	for (int i = 0; i < 2; i++) {
@@ -63,31 +64,39 @@ int main(int argc, char** argv, char** envp) {
 			assert(subs == 0);
 
 			node2->addSubscriber(sub1);
+			std::cout << "Waiting for 1st subscriber" << std::endl;
 			subs = pub->waitForSubscribers(1);
 			assert(subs == 1);
 
 			node2->addSubscriber(sub2);
+			std::cout << "Waiting for 2nd subscriber" << std::endl;
 			subs = pub->waitForSubscribers(2);
 			assert(subs == 2);
 
 			node2->addSubscriber(sub3);
+			std::cout << "Waiting for 3rd subscriber" << std::endl;
 			subs = pub->waitForSubscribers(3);
 			assert(subs == 3);
 
 			node2->removeSubscriber(sub1);
 			Thread::sleepMs(50);
+			std::cout << "Removed 1st subscriber" << std::endl;
 			subs = pub->waitForSubscribers(0);
 			assert(subs == 2);
 
 			node2->removeSubscriber(sub2);
+			std::cout << "Removed 2nd subscriber" << std::endl;
 			Thread::sleepMs(50);
 			subs = pub->waitForSubscribers(0);
 			assert(subs == 1);
 
 			node2->removeSubscriber(sub3);
+			std::cout << "Removed 3rd subscriber" << std::endl;
 			Thread::sleepMs(50);
 			subs = pub->waitForSubscribers(0);
 			assert(subs == 0);
+
+			std::cout << "Successfully connected subscribers to publishers" << std::endl;
 
 			node2->removeSubscriber(sub1);
 			node2->removeSubscriber(sub2);

5118a24963bb219f53ad800ece4c42807f5302c6
Stefan Radomski <radomski@tk.informatik.tu-darmstadt.de>
2012-10-11 15:46:58 +0200

    used bcp from boost to update boost headers

---
+++ contrib/local/bcp-boost.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+ME=`basename $0`
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+CWD=`pwd`
+
+SOURCE_FILES=`find ${DIR}/../../core/ -name \*.h -print -o -name \*.cpp -print`
+echo ${SOURCE_FILES}
+
+/Users/sradomski/Documents/TK/Code/boost_1_51_0/dist/bin/bcp \
+--boost=/Users/sradomski/Documents/TK/Code/boost_1_51_0 \
+--scan ${SOURCE_FILES} \
+${DIR}/../prebuilt/include
+
+rm -rf ${DIR}/../prebuilt/include/libs

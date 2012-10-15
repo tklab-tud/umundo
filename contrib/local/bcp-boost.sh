9b8f6772ff6f0a05d404f5e78fdfa1fdcd109e66
Stefan Radomski <radomski@tk.informatik.tu-darmstadt.de>
2012-10-16 00:33:06 +0200

    Surpress list of files

--- contrib/local/bcp-boost.sh
+++ contrib/local/bcp-boost.sh
@@ -5,7 +5,7 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 CWD=`pwd`
 
 SOURCE_FILES=`find ${DIR}/../../core/ -name \*.h -print -o -name \*.cpp -print`
-echo ${SOURCE_FILES}
+#echo ${SOURCE_FILES}
 
 /Users/sradomski/Documents/TK/Code/boost_1_51_0/dist/bin/bcp \
 --boost=/Users/sradomski/Documents/TK/Code/boost_1_51_0 \

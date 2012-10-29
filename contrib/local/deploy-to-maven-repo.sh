d43a4c4998bb83415e9966833cd422b7f4347237
Stefan Radomski <radomski@tk.informatik.tu-darmstadt.de>
2012-10-29 12:39:00 +0100

    Correctly identify compiler again

--- contrib/local/deploy-to-maven-repo.sh
+++ contrib/local/deploy-to-maven-repo.sh
@@ -2,9 +2,9 @@
 #
 # Deploy umundo.jar into maven repository
 #
-mvn2 deploy:deploy-file -DgroupId=org.umundo -DartifactId=umundocore -Dversion=0.1.1 \
+mvn2 deploy:deploy-file -DgroupId=org.umundo -DartifactId=umundocore -Dversion=0.1.2-pre2 \
   -DgeneratePom=true \
   -Dpackaging=jar \
-  -Dfile=umundo.jar \
+  -Dfile=../../package/umundo.jar \
   -DrepositoryId=tu.darmstadt.umundo \
   -Durl=scp://umundo.tk.informatik.tu-darmstadt.de/var/www/html/maven2

be722e4174a3f615d81cef2d8236e0fb4e48a255
Stefan Radomski <radomski@tk.informatik.tu-darmstadt.de>
2012-12-20 22:32:31 +0100

    Adapted to new astyle syntax

--- contrib/beautifyCode.sh
+++ contrib/beautifyCode.sh
@@ -12,29 +12,29 @@ CWD=`pwd`
 astyle  \
 	--style=java \
 	--indent=tab \
-	--recursive ${DIR}/../core/*.cpp ${DIR}/../core/*.h
+	--recursive "${DIR}/../core/*.cpp" "${DIR}/../core/*.h"
 find ${DIR}/../core/ -iname '*.orig' -exec rm {} \;
 
 astyle  \
 	--style=java \
 	--indent=tab \
-	--recursive ${DIR}/../s11n/*.cpp ${DIR}/../s11n/*.h
+	--recursive "${DIR}/../s11n/*.cpp" "${DIR}/../s11n/*.h"
 find ${DIR}/../s11n/ -iname '*.orig' -exec rm {} \;
 
 astyle  \
 	--style=java \
 	--indent=tab \
-	--recursive ${DIR}/../rpc/*.cpp ${DIR}/../rpc/*.h
+	--recursive "${DIR}/../rpc/*.cpp" "${DIR}/../rpc/*.h"
 find ${DIR}/../rpc/ -iname '*.orig' -exec rm {} \;
 
 astyle  \
 	--style=java \
 	--indent=tab \
-	--recursive ${DIR}/../util/*.cpp ${DIR}/../util/*.h
+	--recursive "${DIR}/../util/*.cpp" "${DIR}/../util/*.h"
 find ${DIR}/../util/ -iname '*.orig' -exec rm {} \;
 
 astyle  \
 	--style=java \
 	--indent=tab \
-	--recursive ${DIR}/../apps/*.cpp ${DIR}/../apps/*.h
+	--recursive "${DIR}/../apps/*.cpp" "${DIR}/../apps/*.h"
 find ${DIR}/../apps/ -iname '*.orig' -exec rm {} \;

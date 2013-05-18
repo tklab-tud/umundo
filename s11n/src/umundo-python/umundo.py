9ad087897111ed52d9f0a8f5217555b0f1d9edb6
Marcel Blöcher <marcel@bloecherweb.de>
2013-05-18 20:06:03 +0200

    s11n protobuf python module

--- s11n/src/umundo-python/umundo.py
+++ s11n/src/umundo-python/umundo.py
@@ -1,12 +1,64 @@
+import sys
 
-class TypedPublisher:
-    pass
+try:
+    import umundo64 as umundo_proto
+except:
+    import umundo as umundo_proto
 
-class TypedSubscriber:
-    pass
+
+class TypedPublisher(umundo_proto.Publisher):
+    def prepareMessage(self, protoType, messageLiteObject):
+        msg = umundo_proto.Message()
+        buffer = messageLiteObject.SerializeToString()
+        msg.setData(buffer)
+        msg.putMeta("um.s11n.type", protoType)
+        return msg
+
+    def sendObject(self, protoType, messageLiteObject):
+		self.send(self.prepareMessage(protoType, messageLiteObject))
+
+
+
+class TypedSubscriber(umundo_proto.Subscriber):
+
+    def __init__(self, channel, receiver):
+        umundo_proto.Subscriber.__init__(self, channel)
+        self.channel = channel
+        self.receiver = receiver
+        decoratedReceiver = TypedSubscriber.DeserializingReceiverDecorator(receiver)
+
+        self.setReceiver(decoratedReceiver)
+
+    class DeserializingReceiverDecorator(umundo_proto.Receiver):
+
+        def __init__(self, receiver):
+            self.receiver = receiver
+
+        def receive(self, msg):
+            print(msg)
+            if (not "um.s11n.type" in msg.getMeta()):
+                return self.receiver.receiveObject(msg)
+            protoType = msg.getMeta("um.s11n.type");
+            data = msg.getData();
+
+            print("type: "+protoType)
+            print("data: "+data)
+            return None
+
+
 
 class TypedReceiver:
-    pass
+    def receiveObject(obj, msg):
+        pass
+
 
 class TypedGreeter:
-    pass
+    def welcome(self, atPub, nodeId, subId):
+        pass
+
+    def farewell(self, fromPub, nodeId, subId):
+        pass
+
+    def prepareMessage(self, type, msgLite):
+        pass
+

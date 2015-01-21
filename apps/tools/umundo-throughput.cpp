29218c61657308cf75c963b694502fedc7cf5a00
Stefan Radomski <radomski@tk.informatik.tu-darmstadt.de>
2015-01-21 12:44:02 +0100

    Updates for umundo-throughput

--- apps/tools/umundo-throughput.cpp
+++ apps/tools/umundo-throughput.cpp
@@ -35,6 +35,7 @@ size_t currSeqNr = 0;
 
 // server
 size_t bytesPerSecond = 1024 * 1024;
+size_t fixedBytesPerSecond = 0;
 size_t mtu = 1280;
 double pcntLossOk = 0;
 size_t waitForSubs = 0;
@@ -44,7 +45,9 @@ size_t packetsWritten = 0;
 size_t bytesRcvd = 0;
 size_t pktsRecvd = 0;
 size_t lastSeqNr = 0;
+size_t lastTimeStamp = 0;
 size_t pktsDropped = 0;
+Publisher reporter;
 
 class Report {
 public:
@@ -52,6 +55,7 @@ public:
 	size_t pktsDropped;
 	size_t bytesRcvd;
 	size_t pktsLate;
+	size_t latency;
 	double pcntLoss;
 	std::string name;
 };
@@ -65,7 +69,8 @@ class ThroughputReceiver : public Receiver {
 		pktsRecvd++;
 		
 		currSeqNr = strTo<size_t>(msg->getMeta("seqNr"));
-		
+		size_t currTimeStamp = strTo<size_t>(msg->getMeta("now"));
+
 		if (currSeqNr < lastSeqNr)
 			lastSeqNr = 0;
 			
@@ -74,6 +79,26 @@ class ThroughputReceiver : public Receiver {
 		}
 		
 		lastSeqNr = currSeqNr;
+		
+		// reply?
+		if (currTimeStamp - 1000 >= lastTimeStamp)
+		{
+			RScopeLock lock(mutex);
+			Message* msg = new Message();
+			msg->putMeta("bytes.rcvd", toStr(bytesRcvd));
+			msg->putMeta("pkts.dropped", toStr(pktsDropped));
+			msg->putMeta("pkts.rcvd", toStr(pktsRecvd));
+			msg->putMeta("last.seq", toStr(lastSeqNr));
+			msg->putMeta("last.timestamp", toStr(currTimeStamp));
+			reporter.send(msg);
+			delete msg;
+			
+			lastTimeStamp = currTimeStamp;
+			pktsDropped = 0;
+			pktsRecvd = 0;
+			bytesRcvd = 0;
+		}
+
 	}
 };
 
@@ -90,7 +115,10 @@ public:
 		report.pktsRcvd    = strTo<size_t>(msg->getMeta("pkts.rcvd"));
 		report.pktsDropped = strTo<size_t>(msg->getMeta("pkts.dropped"));
 		report.bytesRcvd   = strTo<size_t>(msg->getMeta("bytes.rcvd"));
-
+//		std::cout << Thread::getTimeStampMs() << std::endl;
+//		std::cout << msg->getMeta("last.timestamp") << std::endl;
+		report.latency     = Thread::getTimeStampMs() - strTo<size_t>(msg->getMeta("last.timestamp"));
+		
 		size_t lastSeqNr   = strTo<size_t>(msg->getMeta("last.seq"));
 		report.pktsLate = currSeqNr - lastSeqNr;
 			
@@ -123,6 +151,7 @@ void printUsageAndExit() {
 	printf("Options\n");
 	printf("\t-c                 : act as a client\n");
 	printf("\t-s                 : act as a server\n");
+	printf("\t-f BYTES/s         : try to maintain given throughput\n");
 	printf("\t-l                 : acceptable packet loss in percent\n");
 	printf("\t-m <number>        : MTU to use on server (defaults to 1280)\n");
 	printf("\t-w <number>        : wait for given number of subscribers before testing\n");
@@ -171,7 +200,7 @@ std::string timeToDisplay(size_t ns) {
 
 int main(int argc, char** argv) {
 	int option;
-	while ((option = getopt(argc, argv, "csm:w:l:")) != -1) {
+	while ((option = getopt(argc, argv, "csm:w:l:f:")) != -1) {
 		switch(option) {
 			case 'c':
 				isClient = true;
@@ -185,6 +214,21 @@ int main(int argc, char** argv) {
 			case 'm':
 				mtu = atoi((const char*)optarg);
 				break;
+			case 'f': {
+				std::string arg = optarg;
+				size_t multiply = 1;
+				char suffix = arg[arg.length() - 1];
+				if (suffix == 'K')
+					multiply = 1024;
+				if (suffix == 'M')
+					multiply = 1024 * 1024;
+				if (suffix == 'G')
+					multiply = 1024 * 1024 * 1024;
+				fixedBytesPerSecond = atoi((multiply > 1 ? arg.substr(0, arg.length() - 1).c_str() : arg.c_str()));
+				fixedBytesPerSecond *= multiply;
+				break;
+			
+			}
 			case 'w':
 				waitForSubs = atoi((const char*)optarg);
 				break;
@@ -197,6 +241,9 @@ int main(int argc, char** argv) {
 	if (!isClient && !isServer)
 		printUsageAndExit();
 
+	if (fixedBytesPerSecond > 0)
+		bytesPerSecond = fixedBytesPerSecond;
+	
 	Discovery disc(Discovery::MDNS);
 	Node node;
 	disc.add(node);
@@ -218,9 +265,11 @@ int main(int argc, char** argv) {
 
 		unsigned long bytesWritten = 0;
 
-		uint64_t now = Thread::getTimeStampMs();
+		uint64_t lastReportAt = Thread::getTimeStampMs();
 		while(1) {
+			uint64_t now = Thread::getTimeStampMs();
 			msg->putMeta("seqNr", toStr(++currSeqNr));
+			msg->putMeta("now", toStr(now));
 			pub.send(msg);
 			
 			bytesWritten += dataSize;
@@ -235,13 +284,20 @@ int main(int argc, char** argv) {
 			}
 			
 			// every second we are writing reports
-			if (Thread::getTimeStampMs() - now > 1000) {
+			if (now - lastReportAt > 1000) {
 				RScopeLock lock(mutex);
 				
 //				std::cout << FORMAT_COL << bytesToDisplay(bytesPerSecond) + "/s";
+				std::cout << FORMAT_COL << "byte sent";
+				std::cout << FORMAT_COL << "pkts sent";
+//				std::cout << FORMAT_COL << "delay";
+				std::cout << FORMAT_COL << "scale";
+				std::cout << FORMAT_COL << std::endl;
+
+				
 				std::cout << FORMAT_COL << bytesToDisplay(bytesWritten) + "/s";
 				std::cout << FORMAT_COL << toStr(packetsWritten) + "pkt/s";
-				std::cout << FORMAT_COL << timeToDisplay(delay);
+//				std::cout << FORMAT_COL << timeToDisplay(delay);
 
 				if (reports.size() == 0) {
 					std::cout << FORMAT_COL << "---";
@@ -253,43 +309,45 @@ int main(int argc, char** argv) {
 					// print report messages
 					std::map<std::string, Report>::iterator repIter = reports.begin();
 
-					while(repIter != reports.end()) {
-						const Report& report = repIter->second;
-						// see if we need to decrease bandwidth
-						if (report.pktsLate > packetsWritten / 2) {
-							// client is lagging more than half a second, scale back somewhat
-							decreasePressure = (std::max)(1.1, decreasePressure);
+					if (fixedBytesPerSecond == 0) {
+						while(repIter != reports.end()) {
+							const Report& report = repIter->second;
+							// see if we need to decrease bandwidth
+							if (report.pktsLate > packetsWritten / 2) {
+								// client is lagging more than half a second, scale back somewhat
+								decreasePressure = (std::max)(1.1, decreasePressure);
+							}
+							if (report.pcntLoss > pcntLossOk) {
+								// we lost packages, scale back somewhat
+								decreasePressure = (std::max)((double)report.pktsDropped / report.pktsRcvd, decreasePressure);
+								decreasePressure = (std::min)(decreasePressure, 4.0);
+							}
+							if (report.pktsLate > 3 * packetsWritten) {
+								// queues explode! scale back alot!
+								decreasePressure = (std::max)((double)report.pktsLate / packetsWritten, decreasePressure);
+								decreasePressure = (std::min)(decreasePressure, 4.0);
+							}
+							repIter++;
 						}
-						if (report.pcntLoss > pcntLossOk) {
-							// we lost packages, scale back somewhat
-							decreasePressure = (std::max)((double)report.pktsDropped / report.pktsRcvd, decreasePressure);
-							decreasePressure = (std::min)(decreasePressure, 4.0);
+						
+						if (decreasePressure > 0) {
+							bytesPerSecond *= (1.0 / decreasePressure);
+							bytesPerSecond = (std::max)(bytesPerSecond, (size_t)(1024));
+							std::cout << FORMAT_COL << "down " + toStr(1.0 / decreasePressure);
+						} else {
+							bytesPerSecond *= 1.2;
+							std::cout << FORMAT_COL << "up 1.2";
 						}
-						if (report.pktsLate > 3 * packetsWritten) {
-							// queues explode! scale back alot!
-							decreasePressure = (std::max)((double)report.pktsLate / packetsWritten, decreasePressure);
-							decreasePressure = (std::min)(decreasePressure, 4.0);
-						}
-						repIter++;
-					}
-					
-					if (decreasePressure > 0) {
-						bytesPerSecond *= (1.0 / decreasePressure);
-						bytesPerSecond = (std::max)(bytesPerSecond, (size_t)(1024));
-						std::cout << FORMAT_COL << 1.0 / decreasePressure;
-					} else {
-						bytesPerSecond *= 1.1;
-						std::cout << FORMAT_COL << 1.1;
 					}
 					std::cout << FORMAT_COL << "---";
 					std::cout << std::endl;
-
 					
-					std::cout << FORMAT_COL << "bytes recv";
+					std::cout << FORMAT_COL << "byte recv";
 					std::cout << FORMAT_COL << "pkts recv";
 					std::cout << FORMAT_COL << "pkts loss";
 					std::cout << FORMAT_COL << "% loss";
 					std::cout << FORMAT_COL << "pkts late";
+					std::cout << FORMAT_COL << "latency";
 					std::cout << FORMAT_COL << "name";
 					std::cout << std::endl;
 					
@@ -300,22 +358,26 @@ int main(int argc, char** argv) {
 						std::cout << FORMAT_COL << toStr(report.pktsRcvd) + "pkt/s";
 						std::cout << FORMAT_COL << toStr(report.pktsDropped) + "pkt/s";
 						std::cout << FORMAT_COL << toStr(report.pcntLoss) + "%";
-						std::cout << FORMAT_COL << toStr(report.pktsLate) + "pkt";
-						if (repIter->first.size() >= 6)
+						std::cout << FORMAT_COL << (report.pktsLate > 100000 ? "N/A" : toStr(report.pktsLate) + "pkt");
+						std::cout << FORMAT_COL << (report.latency > 100000 ? "N/A" : toStr(report.latency) + "ms");
+						if (repIter->first.size() >= 6) {
 							std::cout << FORMAT_COL << repIter->first.substr(0, 6);
+						} else {
+							std::cout << FORMAT_COL << repIter->first.substr(0, 6);
+						}
 						std::cout << std::endl;
 
 						repIter++;
 					}
 
-					std::cout << std::endl;
 				}
+				std::cout << std::endl;
 				
 				bytesWritten = 0;
 				packetsWritten = 0;
 				reports.clear();
 				
-				now = Thread::getTimeStampMs();
+				lastReportAt = Thread::getTimeStampMs();
 			}
 			Thread::sleepNs(delay);
 		}
@@ -324,28 +386,14 @@ int main(int argc, char** argv) {
 	} else {
 		ThroughputReceiver tpRcvr;
 		Subscriber sub("throughput", &tpRcvr);
-		
-		Publisher pub("reports");
+		reporter = Publisher("reports");
 		
 		node.addSubscriber(sub);
-		node.addPublisher(pub);
+		node.addPublisher(reporter);
 		
+		// do nothing here, we reply only when we received a message
 		while(true) {
 			Thread::sleepMs(1000);
-			{
-				RScopeLock lock(mutex);
-				Message* msg = new Message();
-				msg->putMeta("bytes.rcvd", toStr(bytesRcvd));
-				msg->putMeta("pkts.dropped", toStr(pktsDropped));
-				msg->putMeta("pkts.rcvd", toStr(pktsRecvd));
-				msg->putMeta("last.seq", toStr(lastSeqNr));
-				pub.send(msg);
-				delete msg;
-				
-				pktsDropped = 0;
-				pktsRecvd = 0;
-				bytesRcvd = 0;
-			}
 		}
 	}
 }
--- examples/ios/umundo-throughput/ViewController.m
+++ examples/ios/umundo-throughput/ViewController.m
@@ -9,36 +9,52 @@
 #import "ViewController.h"
 
 @implementation ViewController
-@synthesize text, timer, sub, pub, bytesRcvd, pktsDropped, pktsRecvd, lastSeqNr, lock;
+@synthesize text, timer, sub, pub, bytesRcvd, pktsDropped, pktsRecvd, lastSeqNr, lastTimeStamp, currTimeStamp, lock;
 
 - (void)addText:(NSString*)theText {
   text.text = [text.text stringByAppendingString:theText];
 }
 
+- (void)clearText {
+	text.text = @"";
+}
+
+
 - (void)received:(NSData*)data withMeta:(NSDictionary*)meta {
 	[lock lock];
 	bytesRcvd += [data length];
 	pktsRecvd++;
-	
+
 	NSString* seqNrStr = [meta valueForKey:@"seqNr"];
-	NSUInteger currSeqNr = [seqNrStr integerValue];
-	
+	NSUInteger currSeqNr = [seqNrStr longLongValue];
+
+	NSString* currTimeStampStr = [meta valueForKey:@"now"];
+	currTimeStamp = [currTimeStampStr longLongValue];
+
 	if (currSeqNr < lastSeqNr)
 		lastSeqNr = 0;
 
 	if (lastSeqNr > 0 && lastSeqNr != currSeqNr - 1) {
-		NSString *nrDropped = [NSString stringWithFormat:@"[!%d]", currSeqNr - lastSeqNr];
-		[self performSelectorOnMainThread:@selector(addText:) withObject:nrDropped waitUntilDone:NO];
 		pktsDropped += currSeqNr - lastSeqNr;
 	}
 	lastSeqNr = currSeqNr;
+	
+	if (currTimeStamp - 1000 >= lastTimeStamp) {
+		[self accumulateStats];
+		lastTimeStamp = currTimeStamp;
+	}
+	
 	[lock unlock];
 }
 
 - (void)accumulateStats {
 	[lock lock];
-	NSString *logMsg = [NSString stringWithFormat:@"[%ikB]", (NSUInteger)(bytesRcvd / 1024)];
-
+	NSMutableString *logMsg = [[NSMutableString alloc] init];
+	[logMsg appendString:[NSString stringWithFormat:@"Byte rcvd: %ikB\n", (NSUInteger)(bytesRcvd / 1024)]];
+	[logMsg appendString:[NSString stringWithFormat:@"Pkts rcvd: %i\n", pktsRecvd]];
+	[logMsg appendString:[NSString stringWithFormat:@"Pkts drop: %i\n", pktsDropped]];
+	[logMsg appendString:[NSString stringWithFormat:@"Last Seq:  %i\n", lastSeqNr]];
+	
 	std::ostringstream bytesRcvdSS;
 	bytesRcvdSS << bytesRcvd;
 
@@ -51,11 +67,16 @@
 	std::ostringstream lastSeqNrSS;
 	lastSeqNrSS << lastSeqNr;
 
+	std::ostringstream currTimeSS;
+	currTimeSS << currTimeStamp;
+
 	umundo::Message* msg = new umundo::Message();
 	msg->putMeta("bytes.rcvd", bytesRcvdSS.str());
 	msg->putMeta("pkts.dropped", pktsDroppedSS.str());
 	msg->putMeta("pkts.rcvd", pktsRecvdSS.str());
 	msg->putMeta("last.seq", lastSeqNrSS.str());
+	msg->putMeta("last.timestamp", currTimeSS.str());
+
 	[pub sendMsg:msg];
 	delete msg;
 	
@@ -63,7 +84,8 @@
 	pktsRecvd = 0;
 	bytesRcvd = 0;
 
-  [self performSelectorOnMainThread:@selector(addText:) withObject:logMsg waitUntilDone:NO];
+	[self performSelectorOnMainThread:@selector(clearText) withObject:nil waitUntilDone:NO];
+	[self performSelectorOnMainThread:@selector(addText:) withObject:logMsg waitUntilDone:NO];
 	[lock unlock];
 
 }
@@ -82,7 +104,7 @@
   text.text = @"";
 	bytesRcvd = 0;
 	lastSeqNr = 0;
-  timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(accumulateStats) userInfo:nil repeats:YES];
+//  timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(accumulateStats) userInfo:nil repeats:YES];
 }
 
 - (void)viewDidUnload

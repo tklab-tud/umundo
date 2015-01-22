9e97008eeb380feba0612bf55fe3d1e95cf27783
Stefan Radomski <radomski@tk.informatik.tu-darmstadt.de>
2015-01-22 21:40:48 +0100

    Argh!

--- rpc/src/umundo/rpc/Service.cpp
+++ rpc/src/umundo/rpc/Service.cpp
@@ -19,7 +19,7 @@
 
 #include "umundo/rpc/Service.h"
 #include "umundo/rpc/ServiceManager.h"
-#include "umundo/rpct/Regex.h"
+#include "umundo/rpc/Regex.h"
 #include <stdlib.h>
 #include <sstream>
 #include <ctype.h>

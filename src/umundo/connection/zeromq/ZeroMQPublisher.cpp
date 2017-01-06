870512135a553f7959403b002bb63be724d714e1
Stefan Radomski <github@mintwerk.de>
2017-01-06 13:20:11 +0100

    No gettimeofday on windows

--- src/umundo/connection/zeromq/ZeroMQPublisher.cpp
+++ src/umundo/connection/zeromq/ZeroMQPublisher.cpp
@@ -22,10 +22,31 @@
 #include <WinSock2.h>
 #include <ws2tcpip.h>
 #include <Windows.h>
+
+int gettimeofday(struct timeval * tp, struct timezone * tzp)
+{
+    // Note: some broken versions only have 8 trailing zero's, the correct epoch has 9 trailing zero's
+    // This magic number is the number of 100 nanosecond intervals since January 1, 1601 (UTC)
+    // until 00:00:00 January 1, 1970 
+    static const uint64_t EPOCH = ((uint64_t) 116444736000000000ULL);
+
+    SYSTEMTIME  system_time;
+    FILETIME    file_time;
+    uint64_t    time;
+
+    GetSystemTime( &system_time );
+    SystemTimeToFileTime( &system_time, &file_time );
+    time =  ((uint64_t)file_time.dwLowDateTime )      ;
+    time += ((uint64_t)file_time.dwHighDateTime) << 32;
+
+    tp->tv_sec  = (long) ((time - EPOCH) / 10000000L);
+    tp->tv_usec = (long) (system_time.wMilliseconds * 1000);
+    return 0;
+}
+
 #endif
 
 #include "umundo/connection/zeromq/ZeroMQPublisher.h"
-
 #include "umundo/connection/zeromq/ZeroMQNode.h"
 #include "umundo/Message.h"
 #include "umundo/UUID.h"
@@ -34,9 +55,10 @@
 #if defined UNIX || defined IOS || defined IOSSIM
 #include <string.h> // strlen, memcpy
 #include <stdio.h> // snprintf
+#include <sys/time.h> // gettimeofday
+
 #endif
 
-#include <sys/time.h> // gettimeofday
 
 #define um_timersub(a, b, result)                                             \
   do {                                                                        \
